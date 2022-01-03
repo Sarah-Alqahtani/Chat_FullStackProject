470152912647c90f7935e932b9f01b4f51c29be6
Sarah-Alqahtani <91519771+Sarah-Alqahtani@users.noreply.github.com>
2022-01-03 19:26:18 +0000

    Initial Commit

---
+++ Chat_FullStackProjectTests/Chat_FullStackProjectTests.swift
@@ -0,0 +1,33 @@
+//
+//  Chat_FullStackProjectTests.swift
+//  Chat_FullStackProjectTests
+//
+//  Created by administrator on 03/01/2022.
+//
+
+import XCTest
+@testable import Chat_FullStackProject
+
+class Chat_FullStackProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
